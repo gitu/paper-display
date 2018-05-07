7824f0a176f75bdbc2300389a437ba1d0b806d9e
Florian Schrag <florian@schrag.org>
2018-05-07 20:33:44 +0200

    do diff...

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"golang.org/x/image/bmp"
 	"github.com/gitu/paper-display/epd"
 	"time"
+	"image"
 )
 
 func main() {
@@ -15,6 +16,7 @@ func main() {
 
 	url := "https://paper-display.herokuapp.com/clock"
 
+	previous := image.Image(image.NewRGBA(image.Rect(0, 0, 1, 1)))
 	for {
 		response, err := http.Get(url)
 		if err != nil {
@@ -29,8 +31,29 @@ func main() {
 			return
 		}
 
-		display.DisplayImage(image)
+		if !sameImage(previous, image) {
+			fmt.Println("new Image")
+			previous = image
+			display.DisplayImage(image)
+		} else {
+			fmt.Println("same image - skip update")
+		}
 
-		time.Sleep(10 * time.Second)
+		time.Sleep(5 * time.Second)
 	}
+}
+func sameImage(i1 image.Image, i2 image.Image) bool {
+	if i1.Bounds() != i2.Bounds() {
+		return false
+	}
+	for y := 0; y < i1.Bounds().Dy(); y++ {
+		for x := 0; x < i1.Bounds().Dx(); x++ {
+			r1, g1, b1, _ := i1.At(x, y).RGBA()
+			r2, g2, b2, _ := i2.At(x, y).RGBA()
+			if r1 != r2 || g1 != g2 || b1 != b2 {
+				return false
+			}
+		}
+	}
+	return true
 }
